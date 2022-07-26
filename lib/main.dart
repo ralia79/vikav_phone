19d03f2acd897983e83b72f6adbb56f87fa108a1
ralia <ali.roshan.nia.1379@gmail.com>
2022-07-26 09:32:41 +0430

    install get

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+
+
+void main(List<String> args) {
+  runApp(MaterialApp(home: MyApp(),));
+}
+
+
+
+class MyApp extends StatefulWidget {
+  const MyApp({ Key? key }) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      body: Container(
+        color: Colors.red,
+      ),
+    );
+  }
+}
